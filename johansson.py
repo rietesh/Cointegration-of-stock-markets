7cc8b80385132e379c9a0329c60a2a82c95a9c1e
Rietesh <rietesh4535@iitg.ernet.in>
2019-08-24 18:33:19 +0530

    Update johansson.py

--- johansson.py
+++ johansson.py
@@ -1,15 +1,21 @@
 import pandas as pd
 from statsmodels.tsa.vector_ar.vecm import coint_johansen
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import make_interp_spline, BSpline
 
-df_IND = pd.read_csv("final-US.csv")
-df_IND = df_IND.open
-df_US = pd.read_csv("final-HK.csv")
-df_US = df_US.open
+df_1 = pd.read_csv("final-US.csv")
+df_2 = pd.read_csv("final-IND.csv")
 
-df = pd.DataFrame({'x':df_IND,"y":df_US})
+df_1 = np.log(df_1.open)
+df_2 = np.log(df_2.open)
+df = pd.DataFrame({'x':df_1,"y":df_2})
 df = df.fillna(0)
 
 result = coint_johansen(df,0,1)
+
+print('---------------------------------------------------')
+print('IND-US')
 print ('--------------------------------------------------')
 print ('--> Trace Statistics')
 print ('variable statistic Crit-90% Crit-95%  Crit-99%')
