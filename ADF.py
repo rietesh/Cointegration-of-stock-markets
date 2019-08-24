3b30ece99f820310bfd7c5cd0831638e0032e29e
Rietesh <rietesh4535@iitg.ernet.in>
2019-08-24 18:32:26 +0530

    Update ADF.py

--- ADF.py
+++ ADF.py
@@ -3,10 +3,17 @@ import numpy as np
 from pandas import Series
 from statsmodels.tsa.stattools import adfuller
 
+def difference(dataset, interval=1):
+	diff = list()
+	for i in range(interval, len(dataset)):
+		value = dataset[i] - dataset[i - interval]
+		diff.append(value)
+	return Series(diff)
+
 series = pd.read_csv('final-HK.csv')
 X = series.open
-
-X = np.log(X)
+# X = np.log(X)
+X = difference(X)
 result = adfuller(X)
 
 print('ADF Statistic: %f' % result[0])
@@ -14,3 +21,4 @@ print('p-value: %f' % result[1])
 print('Critical Values:')
 for key, value in result[4].items():
 	print('\t%s: %.3f' % (key, value))
+
